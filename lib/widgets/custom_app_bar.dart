8b6268ccbf422a642a6482244fbef916a06929ed
mo-abujabal <mohamed01279055834@gmail.com>
2023-03-01 02:33:34 +0200

    create notes list view

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'costum_icon_note.dart';
 import 'custom_search_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import 'costum_icon_note.dart';
 import 'custom_app_bar.dart';
+import 'notes_list_view.dart';
 
 class NoteViewBody extends StatelessWidget {
   const NoteViewBody({super.key});
@@ -11,13 +11,17 @@ class NoteViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16),
-      child: Column(children: const [
-        SizedBox(
-          height: 28,
-        ),
-        CustomAppBar(),
-        NoteItem(),
-      ]),
+      child: Column(
+        children: const [
+          SizedBox(
+            height: 28,
+          ),
+          CustomAppBar(),
+          Expanded(
+            child: NotesListView(),
+          ),
+        ],
+      ),
     );
   }
 }
