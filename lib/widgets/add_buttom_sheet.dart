d147e35db0cacc77467d595b6b7d08f44093190c
mo-abujabal <mohamed01279055834@gmail.com>
2023-03-02 02:12:06 +0200

    create edit note view

--- lib/widgets/add_buttom_sheet.dart
+++ lib/widgets/add_buttom_sheet.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 
 class AddNoteButtomSheet extends StatelessWidget {
@@ -6,6 +5,6 @@ class AddNoteButtomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container();
   }
 }
